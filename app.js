d7473fe0a099c5cf3c5670d98cd9d609b763f160
elliercottrell <85800129+elliercottrell@users.noreply.github.com>
2023-01-02 14:07:46 +0000

    Change front page ready for whole site change

---
+++ app.js
@@ -0,0 +1,19 @@
+let countDownDate = new Date("Jan 31, 2023").getTime();
+
+let x = setInterval(function () {
+  let now = new Date().getTime();
+
+  let distance = countDownDate - now;
+
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+  document.getElementById("days").innerHTML = days + " DAYS ";
+
+  document.getElementById("hours").innerHTML = hours + " HOURS ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
